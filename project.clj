f0221360f328145e512975e6817f81fc8a532f17
Lei Wang <tendant@gmail.com>
2020-06-30 15:18:52 -0700

    release 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-kafka-client "0.3.0"
+(defproject simple-kafka-client "0.3.1"
   :description "Not another kafka client libary, instead this is a collection of functions for building kafka client in Clojure."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
