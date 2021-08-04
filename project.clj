0d7b4d6848c2857e316575984f99807581751253
Yangye Zhu <yangye.zhu@wishlife.com>
2021-08-04 13:57:41 -0700

    add cheshire

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.apache.kafka/kafka-clients "2.7.0"]
                  [org.clojure/data.json "2.4.0"]
+                 [cheshire "5.10.0"]
                  [yogthos/config "1.1.8"]]
   :aliases {"utils" ["run" "-m" "simple-kafka.util"]})
